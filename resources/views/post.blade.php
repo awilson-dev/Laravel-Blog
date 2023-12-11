b918a18c143932dc3ca59d93096ac738df16c20e
Allen Wilson <a_m_wilson@icloud.com>
2023-12-11 15:56:53 -0500

    Basic View Setup

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1>My First Post</h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt corporis quis alias sint corrupti voluptas expedita libero aperiam, sed sapiente necessitatibus error, modi, hic delectus ab vel nemo voluptates fugit.</p>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt corporis quis alias sint corrupti voluptas expedita libero aperiam, sed sapiente necessitatibus error, modi, hic delectus ab vel nemo voluptates fugit.</p>
+    </article>
+
+    <article>
+    <h1><a href="/post">My Second Post</a></h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt corporis quis alias sint corrupti voluptas expedita libero aperiam, sed sapiente necessitatibus error, modi, hic delectus ab vel nemo voluptates fugit.</p>
+    </article>
+
+    <article>
+    <h1><a href="/post">My Third Post</a></h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt corporis quis alias sint corrupti voluptas expedita libero aperiam, sed sapiente necessitatibus error, modi, hic delectus ab vel nemo voluptates fugit.</p>
+    </article>
+</body>
