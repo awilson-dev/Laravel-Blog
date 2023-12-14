0968e8207444cdb9cc91e810921378ac9d1b5511
Allen Wilson <a_m_wilson@icloud.com>
2023-12-14 17:29:54 -0500

    Lesson 39
     Query selection for category

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -41,7 +41,7 @@
 
                 <div>
                     <a href="/posts/{{ $post->slug }}"
-                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read
+                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8 whitespace-nowrap">Read
                         More</a>
                 </div>
             </footer>
