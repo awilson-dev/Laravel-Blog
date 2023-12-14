93b37b7c6f24fa3599b02310657d7586f6a6659d
Allen Wilson <a_m_wilson@icloud.com>
2023-12-14 15:56:33 -0500

    Lesson 37
    Search functionality

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -18,10 +18,8 @@
                 <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
 
                 @foreach ($categories as $category)
-                    <x-dropdown-item 
-                    href="/categories/{{ $category->slug }}"
-                    :active='request()->is("categories/{$category->slug}")'
-                    >{{ ucwords($category->name) }}</x-dropdown-item>
+                    <x-dropdown-item href="/categories/{{ $category->slug }}"
+                        :active='request()->is("categories/{$category->slug}")'>{{ ucwords($category->name) }}</x-dropdown-item>
                 @endforeach
             </x-dropdown>
         </div>
@@ -52,7 +50,7 @@
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
                 <input type="text" name="search" placeholder="Find something"
-                    class="bg-transparent placeholder-black font-semibold text-sm">
+                    class="bg-transparent placeholder-black font-semibold text-sm" value="{{ request('search') }}">
             </form>
         </div>
     </div>
