9cd9daef86332e12b95754f39a9cb90ae63534f1
Allen Wilson <a_m_wilson@icloud.com>
2023-12-14 16:40:17 -0500

    Lesson 38
    Clean up search

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,14 +10,25 @@ class Post extends Model
     use HasFactory;
 
     protected $guarded = [];
-    
+
     protected $with = ['category', 'author'];
 
-    public function category() {
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%');
+        });
+    }
+
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
 
-    public function author() {
+    public function author()
+    {
         return $this->belongsTo(User::class, 'user_id');
     }
 }
