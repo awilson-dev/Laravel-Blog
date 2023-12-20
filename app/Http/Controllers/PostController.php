dd882c0f75e492ff1cd21617e1f823c9f8fd3986
Allen Wilson <a_m_wilson@icloud.com>
2023-12-20 14:21:54 -0500

    Lesson 62
    Limit create post to admins

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -22,5 +20,10 @@ class PostController extends Controller
         return view('posts.show', [
             'post' => $post
         ]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
     }
 }
