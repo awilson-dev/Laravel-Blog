97ca21fd4c0701a4dc47b1d593579f6e477732b8
Allen Wilson <a_m_wilson@icloud.com>
2023-12-21 13:52:35 -0500

    Clean up

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -22,7 +22,11 @@ class PostFactory extends Factory
             'user_id' => User::factory(),
             'category_id' => Category::factory(),
             'title' => fake()->sentence(),
+<<<<<<< Updated upstream
             'thumbnail' => 'thumbnails/default',
+=======
+            'thumbnail' => 'thumbnails/default.png',
+>>>>>>> Stashed changes
             'slug' => fake()->slug(),
             'excerpt' => '<p>' . implode('<p></p>', fake()->paragraphs(2)) . '</p>',
             'body' => '<p>' . implode('<p></p>', fake()->paragraphs(6)) . '</p>'
